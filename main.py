c934f9bfa55bdd948c03f127c88ed8974f79a5e2
Markitanov <dmarkitanov@gmail.com>
2018-10-03 13:24:58 +0600

    final

--- main.py
+++ main.py
@@ -1,60 +1,51 @@
 # Маркитанов Денис КБТУ Машинное Обучение 2018
 import re
 import numpy as np
+import scipy.spatial.distance
+import math
 
-def unique(l):
-    n = []
-    for i in l:
-        if i not in n:
-            n.append(i)
-    return n
+def cosDist(x, y): # кастомная функция косинусового расстояния
+    dotProduct = 0
+    powX = 0
+    powY = 0
+    for i in range(len(y)): # пробегаюсь по векторам
+        dotProduct += x[i] * y[i]
+        powX += x[i] * x[i]
+        powY += y[i] * y[i]
+    return 1 - (dotProduct / (math.sqrt(powX) * math.sqrt(powY))) # возвращаю 1 - косинусовую симилярность (в итоге косинусово расстояние), которое считается выражением косинуса скалярного произведения
 
-def file_len(fname):
-    with open(fname) as f:
-        for i, l in enumerate(f):
-            pass
-    return i-1 # костыль ¯\_(ツ)_/¯
 
 with open('sentences.txt', 'r') as f:
   sentences = [] # массив будет хранить все предложения в нижнем регистре (каждое слово есть элемент массива)
-  for line in f:
-    line_list = re.split('[^a-z]', line.strip('\\n\\r.').lower())
-    line_list = list(filter(None, line_list))        
-    sentences.append(line_list) # добавление листа слов предложения в массив
+  for sentence in f:
+    sentenceList = re.split('[^a-z]', sentence.strip('\\n\\r.').lower())
+    sentenceList = list(filter(None, sentenceList))        
+    sentences.append(sentenceList) # добавление листа слов предложения в массив
 
-cat = open('sentences.txt') # открываю файл для токенизации
-a = str(cat.readlines()).lower() # в нижний регистр
+text = open('sentences.txt') # открываю файл для токенизации
+loweredText = str(text.readlines()).lower() # в нижний регистр
 
-token_cat = re.split('[^a-z]', a) # разделил предложения на слова
+token = re.split('[^a-z]', loweredText) # разделил предложения на слова
 
-token = [x for x in token_cat if x != '\n' and x != ''] # удалил лишнее
+token = [x for x in token if x != '\n' and x != ''] # удалил лишнее
 
-#Составьте список всех слов, встречающихся в предложениях. 
-#Сопоставьте каждому слову индекс от нуля до (d - 1), где d — число различных слов в предложениях. 
-#Для этого удобно воспользоваться структурой dict
+uniqueToken = np.unique(token) # вытащил уникальные значения
+keys = [x for x in range (len(uniqueToken))] # список уникальных слов
+dictionary = dict(zip(keys, uniqueToken)) # словарь
 
-unique_token = unique(token) # вытащил уникальные значения
-keys = [x for x in range (len(unique_token))] # список уникальных слов
-dictionary_cats = dict(zip(keys, unique_token)) # словарь
+n = sum(1 for line in open('sentences.txt')) # вытаскиваю количество строк
+# n = file_len('sentences.txt') # вытаскиваем количество строк
+d = len(uniqueToken)
 
-#Создайте матрицу размера n * d, где n — число предложений. 
-#Заполните ее: элемент с индексом (i, j) в этой матрице должен быть равен количеству вхождений j-го слова в i-е предложение. 
-#У вас должна получиться матрица размера 22 * 254.
-
-#Определяем функцию подсчета строк в файле
-
-
-n = file_len ('sentences.txt') # вытаскиваем количество строк
-d = len(unique_token)
-
-word_matrix = np.zeros(shape=(n,d)) # создал матрицу 22 (количество строк) на 255 (все нули)
+matrix = np.zeros(shape=(n,d)) # создал матрицу 22 (количество строк) на 255 (все нули)
 for i in range(n):
   for j in range(d):
-    word_matrix[i][j] = sentences[i].count(dictionary_cats[j]) # ложу в матрицу количество слов из словаря, которые встречаются в предложении
+    matrix[i][j] = sentences[i].count(dictionary[j]) # ложу в матрицу количество слов из словаря, которые встречаются в предложении
 
-import scipy.spatial.distance
-cos_matrix = []
+
+output = []
 for i in range(n):
-  cos_matrix.append(scipy.spatial.distance.cosine(word_matrix[0], word_matrix[i]))
-cos_matrix = np.array(cos_matrix) 
-print (cos_matrix)
+#   output.append(scipy.spatial.distance.cosine(matrix[0], matrix[i]))
+    output.append(cosDist(matrix[0], matrix[i]))
+output = np.array(output) 
+print (output)
